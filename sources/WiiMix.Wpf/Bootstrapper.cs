1d52a00bed249b4e8ed23b934f4a072068ef5385
Kimleng <l.kimleng.surface.kh@hotmail.com>
2016-12-19 15:59:44 +0700

    create startup project and initialize shell for sale and inventory system
    Related Work Items: #18

---
+++ sources/WiiMix.Wpf/Bootstrapper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Practices.ServiceLocation;
+using Microsoft.Practices.Unity;
+using Prism.Unity;
+using System.Windows;
+using WiiMix.Wpf.ViewModels;
+using WiiMix.Wpf.Views;
+
+namespace WiiMix.Wpf
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return ServiceLocator.Current.GetInstance<ShellView>();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+            Application.Current.MainWindow = (ShellView) this.Shell;
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+            Container.RegisterType<object, ShellViewModel>();
+        }
+    }
+}
---
+++ sources/WiiMix.Wpf/ViewModels/ShellViewModel.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Practices.Prism.Mvvm;
+
+namespace WiiMix.Wpf.ViewModels
+{
+    public class ShellViewModel : BindableBase
+    {
+    }
+}
