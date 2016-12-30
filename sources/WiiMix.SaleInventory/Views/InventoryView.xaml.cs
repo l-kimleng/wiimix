51f88eedeed5d7fe7ac540f19caabe38c9a7a763
kimleng2 <kimleng.rin@gmail.com>
2016-12-30 11:19:58 +0700

    clean up and managing resouce dictionaries

--- sources/WiiMix.SaleInventory/Views/InventoryView.xaml.cs
+++ sources/WiiMix.SaleInventory/Views/InventoryView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System.Windows.Controls;
 
 namespace WiiMix.SaleInventory.Views
 {
@@ -10,6 +12,18 @@ namespace WiiMix.SaleInventory.Views
         public InventoryView()
         {
             InitializeComponent();
+        }
+
+        private void HamburgerMenu_OnItemClick(object sender, ItemClickEventArgs e)
+        {
+            HamburgerMenuControl.Content = e.ClickedItem;
+        }
+
+        private async void HamburgerMenu_OnOptionsItemClick(object sender, ItemClickEventArgs e)
+        {
+            var menuItem = e.ClickedItem as HamburgerMenuItem;
+            if (menuItem != null)
+                await this.TryFindParent<MetroWindow>().ShowMessageAsync("", $"You clicked on {menuItem.Label} button");
         }
     }
 }
