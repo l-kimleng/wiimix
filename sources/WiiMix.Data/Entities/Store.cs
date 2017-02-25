cfc50454b3dd4eb0560a6fbdb6ae7f073e648e3d
Kimleng <l.kimleng.surface.kh@hotmail.com>
2017-02-25 22:40:53 +0700

    add store item entity

--- sources/WiiMix.Data/Entities/Store.cs
+++ sources/WiiMix.Data/Entities/Store.cs
@@ -1,14 +1,13 @@
-﻿namespace WiiMix.Data.Entities
+﻿using System.Collections.Generic;
+
+namespace WiiMix.Data.Entities
 {
     public class Store
     {
         public int Id { get; set; }
-        public int ProductId { get; set; }
-        public float Quantity { get; set; }
-        public decimal Price { get; set; }
-        public int StockTransferId { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
 
-        public virtual Product Product { get; set; }
-        public virtual StockTransfer StockTransfer { get; set; }
+        public ICollection<StoreItem> StoreItems { get; set; }
     }
 }
---
+++ sources/WiiMix.Data/Entities/StoreItem.cs
@@ -0,0 +1,14 @@
+﻿namespace WiiMix.Data.Entities
+{
+    public class StoreItem
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public float Quantity { get; set; }
+        public decimal Price { get; set; }
+        public int StockTransferId { get; set; }
+
+        public virtual Product Product { get; set; }
+        public virtual StockTransfer StockTransfer { get; set; }
+    }
+}
